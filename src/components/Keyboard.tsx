bcce57df2f70d5181b99acc2162d8e5c62615c5d
Nutifafa Attor <attorfafa@gmail.com>
2022-06-25 13:20:10 +0000

    feat: implemented keyboard layout

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -32,33 +32,94 @@ const Keyboard = () => {
 		setResult(null)
 	}
 
-    // Perform calculations
+	// Perform calculations
 	const calculate = () => {
-        switch (operation) {
-            case "+":
-                clear()
-                setResult(parseInt(secondNumber) + parseInt(firstNumber))
-                break
-            case "-":
-                clear()
-                setResult(parseInt(secondNumber) - parseInt(firstNumber))
-                break
-            case "*":
-                clear()
-                setResult(parseInt(secondNumber) * parseInt(firstNumber))
-                break
-            case "/":
-                clear()
-                setResult(parseInt(secondNumber) / parseInt(firstNumber))
-                break
-            default:
-                clear()
-                setResult(0)
-                break
+		switch (operation) {
+			case '+':
+				clear()
+				setResult(parseInt(secondNumber) + parseInt(firstNumber))
+				break
+			case '-':
+				clear()
+				setResult(parseInt(secondNumber) - parseInt(firstNumber))
+				break
+			case '*':
+				clear()
+				setResult(parseInt(secondNumber) * parseInt(firstNumber))
+				break
+			case '/':
+				clear()
+				setResult(parseInt(secondNumber) / parseInt(firstNumber))
+				break
+			default:
+				clear()
+				setResult(0)
+				break
 		}
 	}
 
-	return <p>hi</p>
+	return (
+		<>
+			<View style={Styles.row}>
+				<Button title='C' isGray onPress={clear} />
+				<Button
+					title='+/-'
+					isGray
+					onPress={() => handleOperationPress('+/-')}
+				/>
+				<Button
+					title='%'
+					isGray
+					onPress={() => handleOperationPress('%')}
+				/>
+				<Button
+					title='÷'
+					isBlue
+					onPress={() => handleOperationPress('/')}
+				/>
+			</View>
+			<View style={Styles.row}>
+				<Button title='7' onPress={() => handleNumberPress('7')} />
+				<Button title='8' onPress={() => handleNumberPress('8')} />
+				<Button title='9' onPress={() => handleNumberPress('9')} />
+				<Button
+					title='×'
+					isBlue
+					onPress={() => handleOperationPress('*')}
+				/>
+			</View>
+			<View style={Styles.row}>
+				<Button title='4' onPress={() => handleNumberPress('4')} />
+				<Button title='5' onPress={() => handleNumberPress('5')} />
+				<Button title='6' onPress={() => handleNumberPress('6')} />
+				<Button
+					title='-'
+					isBlue
+					onPress={() => handleOperationPress('-')}
+				/>
+			</View>
+			<View style={Styles.row}>
+				<Button title='1' onPress={() => handleNumberPress('1')} />
+				<Button title='2' onPress={() => handleNumberPress('2')} />
+				<Button title='3' onPress={() => handleNumberPress('3')} />
+				<Button
+					title='+'
+					isBlue
+					onPress={() => handleOperationPress('+')}
+				/>
+			</View>
+			<View style={Styles.row}>
+				<Button title='.' onPress={() => handleNumberPress('.')} />
+				<Button title='0' onPress={() => handleNumberPress('0')} />
+				<Button title="⌫" onPress={() => setFirstNumber(firstNumber.slice(0, -1))}/>
+				<Button
+					title='='
+					isBlue
+					onPress={() => calculate()}
+				/>
+			</View>
+		</>
+	)
 }
 
 export default Keyboard
