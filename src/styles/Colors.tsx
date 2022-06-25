9aec7e1653826ef428d005c10976b3e9055d62e3
Nutifafa Attor <attorfafa@gmail.com>
2022-06-25 10:08:10 +0000

    feat: initialised global colors

---
+++ src/styles/Colors.tsx
@@ -0,0 +1,12 @@
+// Object to store global colors
+export const colors = {
+    light: '#f1f2f3',
+    dark: '#17171c',
+    blue: '#4b5efc',
+    btnGray: '#4e505f',
+    btnDark: '#2e2f38',
+    gray: '#747477',
+    black: '#000000',
+    white: '#ffffff',
+    result: '#46d5b2',
+}
